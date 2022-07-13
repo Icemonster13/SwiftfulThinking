473b4f3d41a3a6b1592db52d7c0e5156745bac52
Michael Pascucci <mdpascucci@live.com>
2022-07-13 18:55:15 -0400

    Initial App Setup and Folder Creation

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Michael & Diana Pascucci on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
